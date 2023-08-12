7a2a2f8149976e4defeddb096c0b6d2a21f10873
Dhirendra Singh <115912791+dhirendrasingh25@users.noreply.github.com>
2023-08-12 21:28:04 +0530

    working on Cart

--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { selectItems } from "./cartSlice";
-import { Fragment } from "react";
+import { increment, incrementAsync, selectItems } from "./cartSlice";
 import { Dialog, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
@@ -33,10 +32,17 @@ const products = [
   },
   // More products...
 ];
+
 export function Cart() {
-  const count = useSelector();
   const dispatch = useDispatch();
   const [open, setOpen] = useState(true);
+
+  const items = useSelector(selectItems);
+  const totalAmount = items.reduce(
+    (amount, item) => item.price * item.quantity + amount,
+    0
+  );
+  const totalItems = items.reduce((total, item) => item.quantity + total, 0);
 
   return (
     <>
@@ -48,12 +54,12 @@ export function Cart() {
             </h1>
             <div className="flow-root">
               <ul role="list" className="-my-6 divide-y divide-gray-200">
-                {products.map((product) => (
+                {items.map((product) => (
                   <li key={product.id} className="flex py-6">
                     <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
                       <img
-                        src={product.imageSrc}
-                        alt={product.imageAlt}
+                        src={product.thumbnail}
+                        alt={product.title}
                         className="h-full w-full object-cover object-center"
                       />
                     </div>
@@ -62,12 +68,12 @@ export function Cart() {
                       <div>
                         <div className="flex justify-between text-base font-medium text-gray-900">
                           <h3>
-                            <a href={product.href}>{product.name}</a>
+                            <a href={product.href}>{product.title}</a>
                           </h3>
-                          <p className="ml-4">{product.price}</p>
+                          <p className="ml-4">${product.price}</p>
                         </div>
                         <p className="mt-1 text-sm text-gray-500">
-                          {product.color}
+                          {product.brand}
                         </p>
                       </div>
                       <div className="flex flex-1 items-end justify-between text-sm">
@@ -101,33 +107,34 @@ export function Cart() {
           </div>
 
           <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
-            <div className="flex justify-between text-base font-medium text-gray-900">
+            <div className="flex justify-between my-2 text-base font-medium text-gray-900">
               <p>Subtotal</p>
-              <p>$262.00</p>
+              <p>$ {totalAmount}</p>
+            </div>
+            <div className="flex justify-between my-2 text-base font-medium text-gray-900">
+              <p>Total Items in Cart</p>
+              <p>{totalItems} items</p>
             </div>
             <p className="mt-0.5 text-sm text-gray-500">
               Shipping and taxes calculated at checkout.
             </p>
-            <Link to="/checkout">
-              <div className="mt-6">
-                <a
-                  href="#"
-                  className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-                >
-                  Checkout
-                </a>
-              </div>
-            </Link>
+            <div className="mt-6">
+              <Link
+                to="/checkout"
+                className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+              >
+                Checkout
+              </Link>
+            </div>
             <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
               <p>
                 or
                 <Link to="/">
                   <button
                     type="button"
-                    className="font-medium text-indigo-600 hover:text-indigo-500 p-2"
-                    onClick={() => setOpen(false)}
+                    className="font-medium text-indigo-600 hover:text-indigo-500"
                   >
-                    Continue Shopping Now
+                    Continue Shopping
                     <span aria-hidden="true"> &rarr;</span>
                   </button>
                 </Link>
