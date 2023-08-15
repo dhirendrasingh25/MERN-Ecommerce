c5d59b1dd056c9a82f6a027ece62ebaa0420a001
Dhirendra Singh <115912791+dhirendrasingh25@users.noreply.github.com>
2023-08-15 15:06:46 +0530

    completed  User Profile

--- src/pages/CheckOut.js
+++ src/pages/CheckOut.js
@@ -7,10 +7,8 @@ import {
 } from "../features/cart/cartSlice";
 import { Navigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import {
-  selectLoggedInUser,
-  updateUserAsync,
-} from "../features/auth/authSlice";
+import { updateUserAsync } from "../features/auth/authSlice";
+import { selectUserInfo } from "../features/user/userSlice";
 import { useState } from "react";
 import {
   createOrderAsync,
@@ -26,7 +24,7 @@ function Checkout() {
     formState: { errors },
   } = useForm();
 
-  const user = useSelector(selectLoggedInUser);
+  const user = useSelector(selectUserInfo);
   const items = useSelector(selectItems);
   const currentOrder = useSelector(selectCurrentOrder);
 
