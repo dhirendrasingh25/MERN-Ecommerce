73a0428fc65adcdb28bfc95fb2ee1b064063d44d
Dhirendra Singh <115912791+dhirendrasingh25@users.noreply.github.com>
2023-09-24 02:23:48 +0530

    pised

--- Backend/routes/Product.js
+++ Backend/routes/Product.js
@@ -1,8 +1,17 @@
 const express = require("express");
-const { createProduct, fetchAllProducts } = require("../controller/Product");
+const {
+  createProduct,
+  fetchAllProducts,
+  fetchProductById,
+  updateProduct,
+} = require("../controller/Product");
 
 const router = express.Router();
 
-router.post("/", createProduct).get("/", fetchAllProducts);
+router
+  .post("/", createProduct)
+  .get("/", fetchAllProducts)
+  .get("/:id", fetchProductById)
+  .patch("/:id", updateProduct);
 
 exports.router = router;
